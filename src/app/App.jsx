121e5a3e9f0c1e919693ae8f46507c9fb9121b05
chatinunk97 <chatinun.hobby@gmail.com>
2023-08-30 21:48:18 +0700

    Check

---
+++ src/app/App.jsx
@@ -0,0 +1,19 @@
+import "./App.scss";
+import Header from  "../components/Header"
+import Sidebar from "../components/Sidebar";
+
+function App() {
+  return (
+    <div className="todo">
+      <div className="todo__header">
+        <Header/>
+      </div>
+      <div className="todo__sidebar">
+        <Sidebar/>
+      </div>
+      <div className="todo__content">TodoContent</div>
+    </div>
+  );
+}
+
+export default App;

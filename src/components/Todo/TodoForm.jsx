e46d537f9d4fa051a1120439d2f17f10a57440c1
chatinunk97 <chatinun.hobby@gmail.com>
2023-08-31 12:04:01 +0700

    Finish: Dynamic Error Message displaying

--- src/components/Todo/TodoForm.jsx
+++ src/components/Todo/TodoForm.jsx
@@ -1,18 +1,39 @@
-import styles from './TodoForm.module.scss';
-import { Button } from '../Common/Button/Button';
+import styles from "./TodoForm.module.scss";
+import { Button } from "../Common/Button/Button";
+import { useState } from "react";
 
 function TodoForm(props) {
+  const [isError, setIsError] = useState("true");
+  console.log(isError);
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    if (event.target.value) {
+      setIsError(false);
+      return;
+    }
+    setIsError(true);
+  };
+
   return (
     <form className={styles.todo__form__container}>
       {/*	Body */}
-      <input className={styles.todo__form__input} placeholder='Task Name' />
+      <input
+        onChange={handleChange}
+        className={styles.todo__form__input}
+        placeholder="Task Name"
+      />
 
       {/*Form Footer */}
       <div className={styles.todo__form__footer}>
-        <p className={styles.todo__error}>Title is required</p>
+        {isError ? (
+          <p className={styles.todo__error}>Title is required</p>
+        ) : (
+          <></>
+        )}
+
         <div className={styles.todo__form__buttons}>
-          <Button text='Cancel'  active = {false} />
-          <Button text = {props.textSubmit}  active = {true} />
+          <Button text="Cancel" active={false} />
+          <Button text={props.textSubmit} active={true} />
         </div>
       </div>
     </form>
